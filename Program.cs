85e82c55f30b1298cda027a2656e333dc5bb4360
Tobias Pieber <tobiaspieber@outlook.com>
2023-03-21 20:58:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using CharityScanWebApp.Data;
+using cs.api.charityscan.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// Add WebAPI
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<CharityscanDevContext>(options =>
+        options.UseMySQL(builder.Configuration.GetConnectionString("WebApiDatabase") ?? ""));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CharityScan API v1");
+    c.DocumentTitle = "CharityScanAPI v1";
+    c.RoutePrefix = "api";
+}
+);
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
