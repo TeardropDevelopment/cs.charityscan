54bb7e8577ac4a55bf20c941934dd9d298a30e62
Tobias Pieber <tobiaspieber@outlook.com>
2023-03-22 20:31:58 +0100

    Show result

--- wwwroot/js/initScanner.js
+++ wwwroot/js/initScanner.js
@@ -3,15 +3,13 @@ var lastResult, countResults = 0;
 
 function loadScanner() {
 
-    resultContainer = document.getElementById('qr-reader-results');
-
     const html5QrCode = new Html5Qrcode("qr-reader");
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
         if (decodedText !== lastResult) {
             ++countResults;
             lastResult = decodedText;
             // Handle on success condition with the decoded message.
-            resultContainer.innerHtml = decodedResult;
+            document.getElementById('qr-reader-results').innerHTML = decodedResult;
             console.log(`Scan result ${decodedText}`, decodedResult);
         };
     }
