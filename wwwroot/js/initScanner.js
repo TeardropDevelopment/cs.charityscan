9700bf1828fc420344cba22f780a10b07dc8fab0
Tobias Pieber <tobiaspieber@outlook.com>
2023-03-22 20:04:29 +0100

    Callback

--- wwwroot/js/initScanner.js
+++ wwwroot/js/initScanner.js
@@ -1,14 +1,7 @@
 ﻿var resultContainer;
 var lastResult, countResults = 0;
 
-function onScanSuccess(decodedText, decodedResult) {
-    if (decodedText !== lastResult) {
-        ++countResults;
-        lastResult = decodedText;
-        // Handle on success condition with the decoded message.
-        resultContainer.innerHtml = decodedResult;
-        console.log(`Scan result ${decodedText}`, decodedResult);
-    }
+function onScanSuccess
 }
 
 function loadScanner() {
@@ -16,7 +9,14 @@ function loadScanner() {
     resultContainer = document.getElementById('qr-reader-results');
 
     const html5QrCode = new Html5Qrcode("qr-reader");
-    const qrCodeSuccessCallback = onScanSuccess;
+    const qrCodeSuccessCallback = (decodedText, decodedResult) {
+        if (decodedText !== lastResult) {
+            ++countResults;
+            lastResult = decodedText;
+            // Handle on success condition with the decoded message.
+            resultContainer.innerHtml = decodedResult;
+            console.log(`Scan result ${decodedText}`, decodedResult);
+        };
     const config = { fps: 10, qrbox: 250 };
 
     // If you want to prefer back camera
